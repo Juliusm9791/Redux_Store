f474322fafd04dd6bec0ad14a11bfc97ea0f5944
Julius Markauskas <juliusm9791@gmail.com>
2022-02-03 00:12:05 -0500

    upatates on components

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from 'react-redux';
+// import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  // const {
+  //   image,
+  //   name,
+  //   _id,
+  //   price,
+  //   quantity
+  // } = item;
 
   const { cart } = state
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    const itemInCart = cart.find((cartItem) => cartItem._id === state._id)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
-        _id: _id,
+        _id: state._id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
       idbPromise('cart', 'put', {
@@ -41,16 +44,16 @@ function ProductItem(item) {
 
   return (
     <div className="card px-1 py-1">
-      <Link to={`/products/${_id}`}>
+      <Link to={`/products/${state._id}`}>
         <img
-          alt={name}
-          src={`/images/${image}`}
+          alt={state.name}
+          src={`/images/${state.image}`}
         />
-        <p>{name}</p>
+        <p>{state.name}</p>
       </Link>
       <div>
-        <div>{quantity} {pluralize("item", quantity)} in stock</div>
-        <span>${price}</span>
+        <div>{state.quantity} {pluralize("item", state.quantity)} in stock</div>
+        <span>${state.price}</span>
       </div>
       <button onClick={addToCart}>Add to cart</button>
     </div>
